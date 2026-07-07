d81cc79b4212f18bac4c80609dc714309fcd145d
OKLMARO <mounioma@gmail.com>
2026-07-07 13:08:43 +0200

    ex01 v0.1

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/07 12:06:19 by oamairi           #+#    #+#             */
+/*   Updated: 2026/07/07 12:41:19 by oamairi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class BitcoinExchange
+{
+private:
+	
+public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &obj);
+
+	
+
+	BitcoinExchange	&operator=(const BitcoinExchange &obj);
+
+	~BitcoinExchange();
+};
