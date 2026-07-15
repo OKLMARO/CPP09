b075d6e665d2e37922a0c231f9f4aefb36ea7a33
OKLMARO <mounioma@gmail.com>
2026-07-15 12:26:53 +0200

    ex02 v1.1

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 22:21:05 by oamairi           #+#    #+#             */
-/*   Updated: 2026/07/14 17:37:17 by oamairi          ###   ########.fr       */
+/*   Updated: 2026/07/15 12:26:07 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	PmergeMe::suiteJacobsthal(int n)
 
 std::vector<int>	PmergeMe::buildJacobOrderVec(size_t size)
 {
-	size_t n = 2;
-	std::vector<int> order;
+	size_t				n = 2;
+	std::vector<int>	ordre;
 
 	while ((size_t) suiteJacobsthal(n) < size)
 	{
@@ -32,17 +32,20 @@ std::vector<int>	PmergeMe::buildJacobOrderVec(size_t size)
 		for (int j = curr - 1; j >= prev; j--)
 		{
 			if (j < (int)size)
-				order.push_back(j);
+				ordre.push_back(j);
 		}
 		n++;
 	}
+
 	size_t j = suiteJacobsthal(n - 1);
+
 	while (j < size)
 	{
-		order.push_back(j);
+		ordre.push_back(j);
 		j++;
 	}
-	return order;
+
+	return ordre;
 }
 
 void	PmergeMe::dichotomieVec(std::vector<int> &vec, int n)
@@ -66,17 +69,19 @@ std::vector<int> PmergeMe::fordJohnsonVec(std::vector<int> ctn)
 	if (ctn.size() <= 1)
 		return ctn;
 
-	size_t i = 0;
-	int	impaire = INT_MIN;
-	std::vector<std::pair<int, int> > paire;
+	size_t								i = 0;
+	std::vector<std::pair<int, int> >	paire;
+	int									impaire = INT_MIN;
 
 	while (i + 1 < ctn.size())
 	{
 		paire.push_back(std::make_pair(ctn.at(i), ctn.at(i + 1)));
 		i += 2;
 	}
+
 	if (ctn.size() % 2 == 1)
 		impaire = ctn.at(ctn.size() - 1);
+
 	for (size_t i = 0; i < paire.size(); i++)
 	{
 		if (paire.at(i).first < paire.at(i).second)
@@ -128,8 +133,9 @@ std::vector<int> PmergeMe::fordJohnsonVec(std::vector<int> ctn)
 
 std::deque<int> PmergeMe::buildJacobOrderDeque(size_t size)
 {
-	std::deque<int> order;
-	size_t n = 2;
+	size_t			n = 2;
+	std::deque<int>	ordre;
+
 	while ((size_t)suiteJacobsthal(n) < size)
 	{
 		int curr = suiteJacobsthal(n);
@@ -137,23 +143,26 @@ std::deque<int> PmergeMe::buildJacobOrderDeque(size_t size)
 		for (int j = curr - 1; j >= prev; j--)
 		{
 			if (j < (int)size)
-				order.push_back(j);
+				ordre.push_back(j);
 		}
 		n++;
 	}
+
 	size_t j = suiteJacobsthal(n - 1);
+
 	while (j < size)
 	{
-		order.push_back(j);
+		ordre.push_back(j);
 		j++;
 	}
-	return order;
+
+	return ordre;
 }
 
 void PmergeMe::dichotomieDeque(std::deque<int> &deque, int n)
 {
-	int min = 0;
-	int max = deque.size();
+	int	min = 0;
+	int	max = deque.size();
 
 	while (min < max)
 	{
@@ -171,15 +180,16 @@ std::deque<int> PmergeMe::fordJohnsonDeque(std::deque<int> ctn)
 	if (ctn.size() <= 1)
 		return ctn;
 
-	size_t i = 0;
-	int	impaire = INT_MIN;
-	std::deque<std::pair<int, int> > paire;
+	size_t								i = 0;
+	std::deque<std::pair<int, int> >	paire;
+	int									impaire = INT_MIN;
 
 	while (i + 1 < ctn.size())
 	{
 		paire.push_back(std::make_pair(ctn.at(i), ctn.at(i + 1)));
 		i += 2;
 	}
+
 	if (ctn.size() % 2 == 1)
 		impaire = ctn.at(ctn.size() - 1);
 
